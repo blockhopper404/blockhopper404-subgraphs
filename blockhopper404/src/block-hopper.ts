17c2358c20395e01c6b14fe48d07158681ab56a1
Shayan Shiravani <shayan6959@gmail.com>
2024-04-20 14:28:11 +0330

    Refactor rarity seed entities

--- blockhopper404/src/block-hopper.ts
+++ blockhopper404/src/block-hopper.ts
@@ -1,15 +1,16 @@
+import { Bytes, log } from "@graphprotocol/graph-ts"
 import {
   ApprovalForAll as ApprovalForAllEvent,
   ERC20Approval as ERC20ApprovalEvent,
   ERC20Transfer as ERC20TransferEvent,
   ERC721Approval as ERC721ApprovalEvent,
   ERC721Transfer as ERC721TransferEvent,
-  RaritySeedRemoved as RaritySeedRemovedEvent,
-  RaritySeedSet as RaritySeedSetEvent,
+  ERC721TransferByCaller as ERC721TransferByCallerEvent,
   RoleAdminChanged as RoleAdminChangedEvent,
   RoleGranted as RoleGrantedEvent,
   RoleRevoked as RoleRevokedEvent,
-  Transfer as TransferEvent
+  Transfer as TransferEvent,
+  BlockHopper
 } from "../generated/BlockHopper/BlockHopper"
 import {
   ApprovalForAll,
@@ -24,6 +25,7 @@ import {
   RoleRevoked,
   Transfer
 } from "../generated/schema"
+import { ADDRESS_ZERO } from "./constants"
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
@@ -100,36 +102,44 @@ export function handleERC721Transfer(event: ERC721TransferEvent): void {
   entity.save()
 }
 
-export function handleRaritySeedRemoved(event: RaritySeedRemovedEvent): void {
-  let entity = new RaritySeedRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.caller = event.params.caller
-  entity.from = event.params.from
-  entity.BlockHopper_id = event.params.id
-  entity.seed = event.params.seed
+export function handleERC721TransferByCaller(event: ERC721TransferByCallerEvent): void {
+  let contract = BlockHopper.bind(event.address)
+  let seed = contract.try_getRaritySeed(event.params.id)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRaritySeedSet(event: RaritySeedSetEvent): void {
-  let entity = new RaritySeedSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.caller = event.params.caller
-  entity.to = event.params.to
-  entity.BlockHopper_id = event.params.id
-  entity.seed = event.params.seed
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  if(!seed.value) {
+    log.error("Unable to get rarity seed for {}", [event.params.id.toString()])
+    return;
+  }
+  
+  if (event.params.from == Bytes.fromHexString(ADDRESS_ZERO)) {
+    let seedSetEntity = new RaritySeedSet(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
+    seedSetEntity.caller = event.params.caller
+    seedSetEntity.to = event.params.to
+    seedSetEntity.BlockHopper_id = event.params.id
+    seedSetEntity.seed = seed.value
+  
+    seedSetEntity.blockNumber = event.block.number
+    seedSetEntity.blockTimestamp = event.block.timestamp
+    seedSetEntity.transactionHash = event.transaction.hash
+  
+    seedSetEntity.save()
+  } else if(event.params.to == Bytes.fromHexString(ADDRESS_ZERO)) {
+    let seedRemovedEntity = new RaritySeedRemoved(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
+    seedRemovedEntity.caller = event.params.caller
+    seedRemovedEntity.from = event.params.from
+    seedRemovedEntity.BlockHopper_id = event.params.id
+    seedRemovedEntity.seed = seed.value
+  
+    seedRemovedEntity.blockNumber = event.block.number
+    seedRemovedEntity.blockTimestamp = event.block.timestamp
+    seedRemovedEntity.transactionHash = event.transaction.hash
+  
+    seedRemovedEntity.save()
+  }
 }
 
 export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
